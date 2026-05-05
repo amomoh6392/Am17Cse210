b00579836915a23596fb08acf2b2bde3e64c1b9b
amomoh6392 <amomoh@byupathway.edu>
2026-05-05 09:56:33 +0100

    Programming Exercise 1: Input and Output assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("what is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname} ,{firstname} {lastname}.");
+
+
+
     }
 }
