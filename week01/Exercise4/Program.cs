6c8e3fd793c9ac21ed37f888e535d4ace8cff091
amomoh6392 <amomoh@byupathway.edu>
2026-05-05 19:05:41 +0100

    Programming Exercise 4: Lists and Generics assignment done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int user_number = -1;
+        while (user_number != 0)
+        {
+            Console.Write("please enter a number: ");
+            string user_input = Console.ReadLine();
+            user_number = int.Parse(user_input);
+            if (user_number != 0)
+            {
+                numbers.Add(user_number);
+            }
+        }
+        int sum_numbers = 0;
+        foreach (int number in numbers)
+        {
+            sum_numbers = sum_numbers + number;
+        }
+        int count = numbers.Count;
+
+        double average = (double)sum_numbers / count;
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"Sum: {sum_numbers}");
+        Console.WriteLine($"count: {count}");
+        Console.WriteLine($"average : {average}");
+        Console.WriteLine($"largest number: {largest}");
     }
 }
