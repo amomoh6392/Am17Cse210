858c803069175e98ba8c6d230e18472cd82a6fa6
amomoh6392 <amomoh@byupathway.edu>
2026-05-05 11:21:46 +0100

    Programming Exercise 2: If Statements assignment done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is the grade percent? ");
+        string grade = Console.ReadLine();
+        int score = int.Parse(grade);
+        string letter;
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        int lastdight = score % 10;
+        string sign = "";
+        if (lastdight >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastdight <= 3)
+        {
+            sign = "-";
+        }
+        Console.WriteLine($"you have earned the grade {letter} {sign}");
+        
+        if (score >= 70)
+            Console.WriteLine("congratulations you have passed the course");
+        else 
+            Console.WriteLine("sorry retake the course");
     }
 }
