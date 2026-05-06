b9471c278b753070c9bbb3046e724f03bc741c02
amomoh6392 <amomoh@byupathway.edu>
2026-05-06 16:08:08 +0100

    Programming Exercise 5: Functions assignment done

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+        static void Main(string[] args)
+        {
+            DisplayMessage();
+            string name = PromptUserName();
+
+            int number = PromptUserNumber();
+            int result = SquareNumber(number);
+
+            DisplayResult(result, name);
+
+        }
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("welcome to the program");
+        }
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string username = Console.ReadLine();
+            return username;
+        }
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number? ");
+            int usernumber = int.Parse(Console.ReadLine());
+            return usernumber;
+        }
+        static int SquareNumber(int usernumber)
+        {
+            return usernumber * usernumber;
+        }
+        static void DisplayResult(int result, string username)
+        {
+            Console.WriteLine($"Brother {username}, the square of your number is {result}");
+        }
 }
