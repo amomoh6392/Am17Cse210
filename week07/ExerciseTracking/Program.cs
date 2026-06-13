a89e6039819196b1b39726622c283f6b5a51ff31
amomoh6392 <amomoh@byupathway.edu>
2026-06-13 18:06:28 +0100

    week 7 assignment done

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2026, 6, 1), 30, 5.0));
+        activities.Add(new Cycling(new DateTime(2026, 6, 3), 45, 20.0));
+        activities.Add(new Swimming(new DateTime(2026, 6, 5), 40, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
